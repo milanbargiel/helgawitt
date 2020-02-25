de5a3b42c479529df8b6a11ae110e2d6ddee4564
milanbargiel <milan.bargiel@googlemail.com>
2020-02-25 15:22:42 +0100

    add two click solution to load google map

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,14 @@
     img.removeAttribute('data-src');
   };
 });
+
+// load Google Maps on Click for GDPR compliance
+function createMapScript() {
+	var s = document.createElement('script');
+	s.async = true;
+	s.defer = true;
+	s.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD7ljrs81BUmzYdXsjrcq66UQwu8DBUby0&callback=initMap";
+	document.getElementById('google-map').appendChild(s);
+}
+
+document.querySelector('.js-load-google-maps').addEventListener('click', createMapScript);
